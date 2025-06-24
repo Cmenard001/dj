8db7ea27fd32ad121c04a7924580a23b8ac07de2
Cyprien Ménard <cyprien.menard@reseau.eseo.fr>
2025-06-24 21:02:22 +0200

    Don't forget to initialize time api

--- src/main.c
+++ src/main.c
@@ -1,23 +1,16 @@
+#include "api/printf/printf.h"
+#include "api/time/time.h"
 #include "dj/dj.h"
 #include "dj_tester/dj_tester.h"
-#include "api/printf/printf.h"
-
-// Add this at the top to ensure C linkage if using C++
-#ifdef __cplusplus
-extern "C" {
-#endif
 
 int main(void) __attribute__((used));
 int main(void)
 {
     printfInit(); // Initialize printf functionality
+    time_init();  // Initialize time management
 
     // Initialize the DJ module and run the tests
     dj_init();
     dj_tester_launch_tests();
     return 0;
 }
-
-#ifdef __cplusplus
-}
-#endif
